3f6238414f969078001e0a08ea80aa30460a4a20
Luiz A. T. de Arruda <arrudaluiz89@gmail.com>
2023-06-27 16:07:51 -0300

    feat: add environment variables

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+import dotenv from "dotenv"
 import express from 'express';
+
+dotenv.config({ path: ".env" })
+const PORT = process.env.PORT
 
 const app = express();
 
@@ -6,6 +10,6 @@ app.get('/', (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:${3000}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
