73aebf312b8416e870d79d1083249b3c1eb9d45b
Luiz A. T. de Arruda <arrudaluiz89@gmail.com>
2023-07-04 15:23:22 -0300

    feat: create initial page

---
+++ src/templates/index.js
@@ -0,0 +1,18 @@
+export const main = {
+  app: 'Kanban Board',
+  header: {
+    author: 'Autor',
+    contact: 'Contato',
+    project: 'Projeto',
+    techs: 'Ferramentas'
+  },
+  footer: {
+    description: 'UTFPR-CP - Programação Web 2',
+    author: 'Luiz A. T. de Arruda'
+  }
+};
+
+export const index = {
+  abstract:
+    'Uma abordagem visual com foco no fluxo de trabalho. O Kanban Board é uma ferramenta versátil que auxilia na gestão de tarefas em diversos contextos.'
+};
