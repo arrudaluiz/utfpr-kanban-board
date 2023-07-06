dccc310614ef80ef29c25d574e53575a7ed5cd32
Luiz A. T. de Arruda <arrudaluiz89@gmail.com>
2023-07-06 18:28:00 -0300

    feat: add email sending

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import template from '../templates/index.js';
+import sendMail from '../utils/sendMail.js';
 
 const router = express.Router();
 
@@ -26,6 +27,25 @@ router.get('/project', (req, res) => {
 
 router.get('/techs', (req, res) => {
   res.render('techs', templateObj(template.techs));
+});
+
+router.post('/send-email', async (req, res) => {
+  try {
+    const { name, email, subject, message } = req.body;
+    const text = `Nome: ${name}\r\nE-mail: ${email}\r\nAssunto: ${subject}\r\nMensagem: ${message}`;
+    const templateObj = {
+      ...template.index,
+      title: 'Mensagem enviada!',
+      message: 'Sua mensagem foi enviada e em breve responderemos em seu e-mail.'
+    };
+
+    await sendMail(email, subject, text);
+
+    res.render('mail-sent', templateObj);
+  } catch (error) {
+    console.error('Error sending email:', error);
+    next(error);
+  }
 });
 
 export default router;
---
+++ src/utils/sendMail.js
@@ -0,0 +1,24 @@
+import { createTransport } from 'nodemailer';
+
+export default async function (from, subject, text) {
+  const mailOptions = { from, to: process.env.MAILER_USER, subject, text };
+  const transporter = createTransport({
+    host: process.env.MAILER_HOST,
+    port: +process.env.MAILER_PORT,
+    secure: +process.env.MAILER_PORT === 465,
+    auth: {
+      user: process.env.MAILER_USER,
+      pass: process.env.MAILER_USER_PASSWORD
+    }
+  });
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(info);
+      }
+    });
+  });
+}
